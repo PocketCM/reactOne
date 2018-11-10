d7044103b90ee74c313c2f3b341d9a1d9c1da348
Ashley <anoelj86@yahoo.com>
2018-11-10 13:05:48 -0700

    first

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Logo from "../../Components/Logo";
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            <div>
+
+
+                <h1>Case Manager Dashboard</h1>
+
+
+
+            </div>
+        )
+    }
+}
+
+export default Home; 
