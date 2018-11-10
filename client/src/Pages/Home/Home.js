40020e334d0824e07935b5756c1aed1218dd837b
Ashley <anoelj86@yahoo.com>
2018-11-10 13:26:09 -0700

    home

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import Logo from "../../Components/Logo";
+import Sidebar from "../../Components/Sidebar";
 
 class Home extends Component {
 
+    state = {
+        clients: [],
+
+    }
 
     render() {
         return (
             <div>
-
-
                 <h1>Case Manager Dashboard</h1>
+                {/* <Sidebar
+                    this.state.clients.map(clients => {
+                    return <ClientName
+                    id={client.id}
+                    key={clientInformation.id}
+                    name={client.name}
+                    handleClick={this.handleCLick} />
+                } */}
+
 
 
 
